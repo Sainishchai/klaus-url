d67d7731df8b019bfbfa1d25d8f9a1f42b6999ea
Sainishchai <89191791+Sainishchai@users.noreply.github.com>
2023-09-27 16:45:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -396,6 +396,132 @@ async def delete_all_index_confirm(bot, message):
   await message.message.edit('Succesfully Deleted All The Indexed Files.')
 
 
+@Client.on_message(filters.command("send") & filters.user(ADMINS))
+async def send_msg(bot, message):
+  if message.reply_to_message:
+    target_id = message.text.split(" ", 1)[1]
+    out = "Users Saved In DB Are:\n\n"
+    success = False
+    try:
+      user = await bot.get_users(target_id)
+      users = await db.get_all_users()
+      async for usr in users:
+        out += f"{usr['id']}"
+        out += '\n'
+      if str(user.id) in str(out):
+        await message.reply_to_message.copy(int(user.id))
+        success = True
+      else:
+        success = False
+      if success:
+        await message.reply_text(
+          f"<b>Your message has been successfully send to {user.mention}.</b>")
+      else:
+        await message.reply_text(
+          "<b>This user didn't started this bot yet !</b>")
+    except Exception as e:
+      await message.reply_text(f"<b>Error: {e}</b>")
+  else:
+    await message.reply_text(
+      "<b>Use this command as a reply to any message using the target chat id. For eg: /send userid</b>"
+    )
+
+
+@Client.on_message(filters.command("gsend") & filters.user(ADMINS))
+async def send_chatmsg(bot, message):
+  if message.reply_to_message:
+    target_id = message.text.split(" ", 1)[1]
+    command = ["/gsend"]
+    for cmd in command:
+      if cmd in target_id:
+        target_id = target_id.replace(cmd, "")
+    success = False
+    try:
+      chat = await bot.get_chat(int(target_id))
+      await message.reply_to_message.copy(int(chat.id))
+      success = True
+    except Exception as e:
+      await message.reply_text(f"<b>Eʀʀᴏʀ :- <code>{e}</code></b>")
+    if success:
+      await message.reply_text(
+        f"<b>Yᴏᴜʀ Mᴇssᴀɢᴇ Hᴀs Bᴇᴇɴ Sᴜᴄᴇssғᴜʟʟʏ Sᴇɴᴅ To {chat.id}.</b>")
+    else:
+      await message.reply_text("<b>Aɴ Eʀʀᴏʀ Oᴄᴄᴜʀʀᴇᴅ !</b>")
+  else:
+    await message.reply_text("<b>Cᴏᴍᴍᴀɴᴅ Iɴᴄᴏᴍᴘʟᴇᴛᴇ...</b>")
+
+@Client.on_message(filters.command("shortlink") & filters.user(ADMINS))
+async def shortlink(bot, message):
+    chat_type = message.chat.type
+    if chat_type == enums.ChatType.PRIVATE:
+        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !</b>")
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        grpid = message.chat.id
+        title = message.chat.title
+    else:
+        return
+    data = message.text
+    userid = message.from_user.id
+    user = await bot.get_chat_member(grpid, userid)
+    if user.status != enums.ChatMemberStatus.ADMINISTRATOR and user.status != enums.ChatMemberStatus.OWNER and str(userid) not in ADMINS:
+        return await message.reply_text("<b>You don't have access to use this command !</b>")
+    else:
+        pass
+    try:
+        command, shortlink_url, api = data.split(" ")
+    except:
+        return await message.reply_text("<b>Command Incomplete :(\n\nGive me a shortlink and api along with the command !\n\nFormat: <code>/shortlink shorturllink.in 95a8195c40d31e0c3b6baa68813fcecb1239f2e9</code></b>")
+    reply = await message.reply_text("<b>Please Wait...</b>")
+    await save_group_settings(grpid, 'shortlink', shortlink_url)
+    await save_group_settings(grpid, 'shortlink_api', api)
+    await save_group_settings(grpid, 'is_shortlink', True)
+    await reply.edit_text(f"<b>Successfully added shortlink API for {title}.\n\nCurrent Shortlink Website: <code>{shortlink_url}</code>\nCurrent API: <code>{api}</code></b>")
+
+@Client.on_message(filters.command('set_template'))
+async def save_template(client, message):
+  sts = await message.reply("Checking template")
+  userid = message.from_user.id if message.from_user else None
+  if not userid:
+    return await message.reply(
+      f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+  chat_type = message.chat.type
+
+  if chat_type == enums.ChatType.PRIVATE:
+    grpid = await active_connection(str(userid))
+    if grpid is not None:
+      grp_id = grpid
+      try:
+        chat = await client.get_chat(grpid)
+        title = chat.title
+      except:
+        await message.reply_text("Make sure I'm present in your group!!",
+                                 quote=True)
+        return
+    else:
+      await message.reply_text("I'm not connected to any groups!", quote=True)
+      return
+
+  elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+    grp_id = message.chat.id
+    title = message.chat.title
+
+  else:
+    return
+
+  st = await client.get_chat_member(grp_id, userid)
+  if (st.status != enums.ChatMemberStatus.ADMINISTRATOR
+      and st.status != enums.ChatMemberStatus.OWNER
+      and str(userid) not in ADMINS):
+    return
+
+  if len(message.command) < 2:
+    return await sts.edit("No Input!!")
+  template = message.text.split(" ", 1)[1]
+  await save_group_settings(grp_id, 'template', template)
+  await sts.edit(f"Successfully changed template for {title} to\n\n{template}")
+
+
+
 @Client.on_message(filters.command('settings'))
 async def settings(client, message):
   userid = message.from_user.id if message.from_user else None
@@ -522,127 +648,3 @@ async def settings(client, message):
       disable_web_page_preview=True,
       parse_mode=enums.ParseMode.HTML,
       reply_to_message_id=message.id)
-
-@Client.on_message(filters.command("send") & filters.user(ADMINS))
-async def send_msg(bot, message):
-  if message.reply_to_message:
-    target_id = message.text.split(" ", 1)[1]
-    out = "Users Saved In DB Are:\n\n"
-    success = False
-    try:
-      user = await bot.get_users(target_id)
-      users = await db.get_all_users()
-      async for usr in users:
-        out += f"{usr['id']}"
-        out += '\n'
-      if str(user.id) in str(out):
-        await message.reply_to_message.copy(int(user.id))
-        success = True
-      else:
-        success = False
-      if success:
-        await message.reply_text(
-          f"<b>Your message has been successfully send to {user.mention}.</b>")
-      else:
-        await message.reply_text(
-          "<b>This user didn't started this bot yet !</b>")
-    except Exception as e:
-      await message.reply_text(f"<b>Error: {e}</b>")
-  else:
-    await message.reply_text(
-      "<b>Use this command as a reply to any message using the target chat id. For eg: /send userid</b>"
-    )
-
-
-@Client.on_message(filters.command("gsend") & filters.user(ADMINS))
-async def send_chatmsg(bot, message):
-  if message.reply_to_message:
-    target_id = message.text.split(" ", 1)[1]
-    command = ["/gsend"]
-    for cmd in command:
-      if cmd in target_id:
-        target_id = target_id.replace(cmd, "")
-    success = False
-    try:
-      chat = await bot.get_chat(int(target_id))
-      await message.reply_to_message.copy(int(chat.id))
-      success = True
-    except Exception as e:
-      await message.reply_text(f"<b>Eʀʀᴏʀ :- <code>{e}</code></b>")
-    if success:
-      await message.reply_text(
-        f"<b>Yᴏᴜʀ Mᴇssᴀɢᴇ Hᴀs Bᴇᴇɴ Sᴜᴄᴇssғᴜʟʟʏ Sᴇɴᴅ To {chat.id}.</b>")
-    else:
-      await message.reply_text("<b>Aɴ Eʀʀᴏʀ Oᴄᴄᴜʀʀᴇᴅ !</b>")
-  else:
-    await message.reply_text("<b>Cᴏᴍᴍᴀɴᴅ Iɴᴄᴏᴍᴘʟᴇᴛᴇ...</b>")
-
-@Client.on_message(filters.command("shortlink") & filters.user(ADMINS))
-async def shortlink(bot, message):
-    chat_type = message.chat.type
-    if chat_type == enums.ChatType.PRIVATE:
-        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !</b>")
-    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        grpid = message.chat.id
-        title = message.chat.title
-    else:
-        return
-    data = message.text
-    userid = message.from_user.id
-    user = await bot.get_chat_member(grpid, userid)
-    if user.status != enums.ChatMemberStatus.ADMINISTRATOR and user.status != enums.ChatMemberStatus.OWNER and str(userid) not in ADMINS:
-        return await message.reply_text("<b>You don't have access to use this command !</b>")
-    else:
-        pass
-    try:
-        command, shortlink_url, api = data.split(" ")
-    except:
-        return await message.reply_text("<b>Command Incomplete :(\n\nGive me a shortlink and api along with the command !\n\nFormat: <code>/shortlink shorturllink.in 95a8195c40d31e0c3b6baa68813fcecb1239f2e9</code></b>")
-    reply = await message.reply_text("<b>Please Wait...</b>")
-    await save_group_settings(grpid, 'shortlink', shortlink_url)
-    await save_group_settings(grpid, 'shortlink_api', api)
-    await save_group_settings(grpid, 'is_shortlink', True)
-    await reply.edit_text(f"<b>Successfully added shortlink API for {title}.\n\nCurrent Shortlink Website: <code>{shortlink_url}</code>\nCurrent API: <code>{api}</code></b>")
-
-@Client.on_message(filters.command('set_template'))
-async def save_template(client, message):
-  sts = await message.reply("Checking template")
-  userid = message.from_user.id if message.from_user else None
-  if not userid:
-    return await message.reply(
-      f"You are anonymous admin. Use /connect {message.chat.id} in PM")
-  chat_type = message.chat.type
-
-  if chat_type == enums.ChatType.PRIVATE:
-    grpid = await active_connection(str(userid))
-    if grpid is not None:
-      grp_id = grpid
-      try:
-        chat = await client.get_chat(grpid)
-        title = chat.title
-      except:
-        await message.reply_text("Make sure I'm present in your group!!",
-                                 quote=True)
-        return
-    else:
-      await message.reply_text("I'm not connected to any groups!", quote=True)
-      return
-
-  elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-    grp_id = message.chat.id
-    title = message.chat.title
-
-  else:
-    return
-
-  st = await client.get_chat_member(grp_id, userid)
-  if (st.status != enums.ChatMemberStatus.ADMINISTRATOR
-      and st.status != enums.ChatMemberStatus.OWNER
-      and str(userid) not in ADMINS):
-    return
-
-  if len(message.command) < 2:
-    return await sts.edit("No Input!!")
-  template = message.text.split(" ", 1)[1]
-  await save_group_settings(grp_id, 'template', template)
-  await sts.edit(f"Successfully changed template for {title} to\n\n{template}")
